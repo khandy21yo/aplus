37f4fe1033ecf5691f369ee7c147b989f006d727
kevin <khandy21yo@gmail.com>
2017-10-23 16:36:15 -0600

    Added date_invdcode

--- CMC040/cmcfun.h
+++ CMC040/cmcfun.h
@@ -7,8 +7,9 @@ std::string assg_makebatch(
 	const std::string &given_time);
 void assg_unmakebatch(std::string &batch,
 	std::string &given_date, std::string &given_time);
-std::string date_today(void);
 long date_daycode(const std::string &day);
+std::string date_invdcode(long daycode);
+std::string date_today(void);
 long time_code(const std::string &datum);
 std::string date_invdcode(long dcode);
 std::string time_invcode(long tcode);
---
+++ CMC040/cmcfun/date_invdcode.cc
@@ -0,0 +1,93 @@
+/*	TITLE "Inverse of DATE_DAYCODE Function"
+ */
+#pragma module date_invdcode "V3.6 Calico"
+
+/*
+ *
+ *++
+ *
+ * Abstract:HELP
+ *	.B
+ *	Inverse of DATE__DAYCODE function.
+ *
+ * Index:
+ *
+ * Parameters:
+ *
+ *	DAYCODE
+ *		The passed day code as returned from DATE_DAYCODE.
+ *
+ *	Returned value
+ *		A string in the form YYYYMMDD.
+ *--
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <string>
+
+#include "cmcfun.h"
+
+#define sgn(x) ((x) > 0 ? 1 : 0)
+
+std::string date_invdcode(long daycode)
+{
+	long cd;
+	long cent;
+	long cent21;
+	long yr;
+	long end_yr;
+	long ly;
+	long mon;
+	long day;
+	long jul;
+
+	char makestring[10];
+
+	cd = daycode + 55773;
+
+	cent = (cd - 1) / 36524;
+	if (cd > 146097)
+	{
+		cent21 = 1;
+	}
+	else
+	{
+		cent21 = 0;
+	}
+	cd = cd - cent * 36524 - cent21;
+	yr = floor((cd - cd / 1462.0)/365.0) + 100 * cent + 1600;
+
+	end_yr = sgn(yr % 100);
+
+	jul = daycode - (yr - 1600) * 365 -
+		((yr - 1601) / 4) + 55757 +
+		((yr - 1) / 100) -
+		((yr - 1601) / 400);
+	ly = 1 - end_yr * sgn(yr % 4) -
+		(1 - end_yr) * sgn(yr % 400);
+
+	if (jul < 32)
+	{
+		mon = 1;
+	}
+	else
+	{
+		mon = ((jul - ly - 59) / 30.59 + 2.98);
+	}
+
+	if (mon < 3)
+	{
+		day = jul - 31 * (mon - 1);
+	}
+	else
+	{
+		day = jul - floor((mon - 2.98) * 30.59 + 59) - ly;
+	}
+
+	/*
+	 * Generate and return output string
+	 */
+	sprintf(makestring, "%04ld%02ld%02ld", yr, mon, day);
+	return makestring;
+}
