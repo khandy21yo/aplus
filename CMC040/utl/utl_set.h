dd59821ed2f6f880dcaacdbe9bdcda3605f792c1
kevin <khandy21yo@gmail.com>
2017-11-30 17:57:07 -0700

    Fix utl_set confusion

--- CMC040/utl/utl_set.h
+++ CMC040/utl/utl_set.h
@@ -1,31 +1,59 @@
 //! \file
-//
-// File Layout for: UTL.UTL_SET on 21-May-01
-//
-// CMC Utility Set File
-///
-
-
 //!
-//! \brief utl_set class
+//! \brief CMC Utility Set File
 //!
-class utl_set_cdd
+#ifndef _utl_set_h_
+#define _utl_set_h_
+
+#include "database.h"
+
+//! \brief CMC Utility Set File
+//!
+class utl_set_cdd : public db_rmsindexed_cdd
 {
 public:
-//! Program name
+	//!
+	//! brief Copy into a db_map_cdd into specific class variables
+	//!
+	virtual void copy_tomap(db_map_cdd &dbmap)
+	{
+		dbmap["programname"] = programname;
+		dbmap["item"] = item;
+		dbmap["system"] = system;
+		dbmap["allowund"] = allowund;
+		dbmap["hard"] = hard;
+		dbmap["sdata"] = sdata;
+		dbmap["fdata"] = fdata;
+	}
+	//!
+	//! brief Copy into a db_map_cdd into specific class variables
+	//!
+	virtual void copy_frommap(db_map_cdd &dbmap)
+	{
+		programname = dbmap["programname"];
+		item = dbmap["item"];
+		system = dbmap["system"];
+		allowund = dbmap["allowund"];
+		hard = dbmap["hard"];
+		sdata = dbmap["sdata"];
+		fdata = dbmap["fdata"];
+	}
+
+public:
+	//!vProgram name
 	std::string programname; //[39];
-//! Item number
+	//! Item number
 	std::string item; //[6];
-//! System name
+	// System name
 	std::string system; //[2];
-//! Yes or No Flag for Undefined Input
+	//! Yes or No Flag for Undefined Input
 	std::string allowund; //[1];
-//! Hard/Soft/Field default
+	//!  Hard/Soft/Field default
 	std::string hard; //[1];
-//! Data
+	//! Data
 	std::string sdata; //[30];
-//! Data Format
+	//! Data Format
 	std::string fdata; //[30];
-//! Unused Fiels
-	std::string unused2; //[4];
 };
+
+#endif
